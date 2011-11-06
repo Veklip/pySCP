977a3d4c103d437d92ee77df85d6b89379a1b88d
Plamen Kolev <multyrealm@gmail.com>
2011-11-06 18:02:57 +0200

    transfer_test: implemented single file transfer using the SCP protocol

--- test/transfer_test.py
+++ test/transfer_test.py
@@ -31,6 +31,102 @@ def _connect(user, host) :
         ssh.connect(host, username=user, password=passwd)
     return ssh
 
+def _local_from(ssh, paths_from, path_to) :
+    command = "~/transfer_test.py -t %s 2> log.txt" % path_to['path']
+    stdin, stdout, stderr = ssh.exec_command(command)
+
+    path = paths_from[0]['path']
+    fo = open(path, 'rb')
+    stat = os.stat(path)
+    bytes_to_send = stat.st_size
+    # command: sending file
+    # TODO: use mode
+    stdin.write('C0755 %s %s\n' % (bytes_to_send, os.path.basename(path_to['path'])))
+    stdin.flush()
+    ret = stdout.read(1)
+    if ret != '\0' :
+        print >> sys.stderr, "Error:", stdout.readline()
+        return
+
+    # command: beginning data transfer
+    stdin.write('\0')
+    stdin.flush()
+    while True :
+        buf = fo.read(4096)
+        if len(buf) :
+            stdin.write(buf)
+            bytes_to_send = bytes_to_send - len(buf)
+        if bytes_to_send <= 0 :
+            stdin.flush()
+            break
+    fo.close()
+    print >> sys.stderr, "finished transfer"
+    ret = stdout.read(1)
+    if ret != '\0' :
+        print >> sys.stderr, "Error:", stdout.read()
+        return
+    print >> sys.stderr, "sending end signal"
+    # command: end of file data transfer
+    stdin.write('E\n')
+    ret = stdout.read(1)
+    if ret != '\0' :
+        print >> sys.stderr, "Error:", stdout.read()
+        return
+    print >> sys.stderr, "recieved end signal"
+
+def _remote_from(paths_from, path_to) :
+    return 0
+
+def _local_to(ssh, paths_from, path_to) :
+    return 0
+
+def _remote_to(paths_from, path_to) :
+    command = sys.stdin.readline()
+    # TODO: add regex check on the command format
+    if command[0] != 'C' :
+        sys.stdout.write('\2')
+        sys.stdout.write('Unknown command %s\n' % command)
+        sys.stdout.flush()
+        return
+    else :
+        sys.stdout.write('\0')
+        sys.stdout.flush()
+
+    # ignore the '\n' at the end
+    mode, size, path = command[:-1].split(' ', 3)
+    size = int(size)
+
+    command = sys.stdin.read(1)
+    if command != '\0' :
+        print >> sys.stderr, "Unknown protocol command sequence"
+        return
+
+    fo = open(path, 'wb')
+    while True :
+        buf = sys.stdin.read(4096)
+        if len(buf) :
+            fo.write(buf)
+            size = size - len(buf)
+        if size <= 0 :
+            fo.flush()
+            break
+    fo.close()
+    print >> sys.stderr, "finished transfer"
+    sys.stdout.write('\0')
+    sys.stdout.flush()
+    print >> sys.stderr, "sent end transfer signal"
+
+    command = sys.stdin.readline()
+    if command[0] != 'E' :
+        sys.stdout.write('\2')
+        sys.stdout.write('Unknown command %s\n' % command)
+        sys.stdout.flush()
+        return
+    else :
+        sys.stdout.write('\0')
+        sys.stdout.flush()
+    print >> sys.stderr, "recieved end file data command"
+
 def _build_arg_parser() :
     parser = argparse.ArgumentParser(description="file transfer test script", prog="transfer_test")
     parser.add_argument("paths", action="store", nargs="+", default=None, help="([[user@]host:]/path/to/file){2,}")
@@ -57,26 +153,57 @@ def _parse_paths(paths) :
 # TODO: check if all input and output files are from the same hosts
 def _analyse_paths(paths) :
     if paths[0]['host'] != 'local' :
+        send = False
         path = paths[0]
     elif paths[-1]['host'] != 'local' :
+        send = True
         path = paths[-1]
     else :
         print >> sys.stderr, "Error: no remote host"
         exit(1)
 
     if path['user'] != 'current' :
-        return path['user'], path['host']
+        return send, path['user'], path['host']
     else :
-        return getpass.getuser(), path['host']
+        return send, getpass.getuser(), path['host']
+
+def _check_paths(paths) :
+    ok = True
+    for path in paths :
+        if not os.path.exists(path['path']) :
+            ok = False
+            print >> sys.stderr, "Error: '%s' doesn't exist" % path['path']
+
+    return ok
 
 if __name__ == "__main__" :
     parser = _build_arg_parser()
     args = parser.parse_args(sys.argv[1:])
-    if len(args.paths) < 2 :
-        parser.print_help()
-        exit(1)
     paths = _parse_paths(args.paths)
-    user, host = _analyse_paths(paths)
-    ssh = _connect(user, host)
-    ssh.close()
+
+    # these should only be called by the remote
+    if args.from_v :
+        _remote_from(paths[:-1], paths[-1])
+    elif args.to_v :
+        try :
+            _remote_to(paths[:-1], paths[-1])
+        except Exception as ex :
+            print >> sys.stderr, "exception", ex.args, ex.message
+    else :
+        # only executed by the local
+        if len(paths) < 2 :
+            parser.print_help()
+            exit(1)
+        send, user, host = _analyse_paths(paths)
+
+        ssh = _connect(user, host)
+
+        if send :
+            # from local to remote
+            _local_from(ssh, paths[:-1], paths[-1])
+        else :
+            # from remove to local
+            _local_to(ssh, paths[:-1], paths[-1])
+
+        ssh.close()
     exit(0)
