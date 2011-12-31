f9fef67e31130b85b8f1cee115a8f5b7a9ea2b19
Plamen Kolev <multyrealm@gmail.com>
2011-12-31 19:11:55 +0200

    added new error for wrong command format

--- error.py
+++ error.py
@@ -5,12 +5,14 @@ E_END = 1           # end of transfer
 E_RDY = 2           # missing start signal
 E_UNK = 3           # unknown command
 E_TFR = 4           # transfer error
+E_FMT = 5           # command format error
 
 err_str = {E_OK :'No errors\n',
            E_END:'End of transfer/directory\n',
            E_RDY:'Receiving end not ready yet\n',
            E_UNK:'Protocol error\n',
-           E_TFR:'Transfer error\n'}
+           E_TFR:'Transfer error\n',
+           E_FMT:'Wrong command format\n'}
 
 def errstr(errno) :
     return err_str[errno]
