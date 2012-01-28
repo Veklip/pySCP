cb47330a843121d06fdda94f9cf7a62e6791a496
Plamen Kolev <multyrealm@gmail.com>
2012-01-28 17:07:59 +0200

    writing the password now allows three tries

--- connector.py
+++ connector.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
 
 import os
+import sys
 import getpass
 import paramiko
 import policies
+
+def _connect_with_password(ssh, user, host, dport=22) :
+    triesLeft = 3
+    while triesLeft :
+        try :
+            passwd = getpass.getpass()
+            ssh.connect(host, port=dport, username=user, password=passwd)
+        except paramiko.BadAuthenticationType as ex :
+            if 'password' in ex.allowed_types or \
+               'keyboard-interactive' in ex.allowed_types :
+                sys.stderr.write("Wrong password.\n")
+                triesLeft -= 1
+                continue
+            sys.stderr.write("Bad Authentication: allowed types: %s\n" \
+                             % (','.join(ex.allowed_types)))
+            return False
+        return True
+    else :
+        return False
 
 def get_connection(user, host, dport=22) :
     ssh = paramiko.SSHClient()
@@ -11,9 +31,11 @@ def get_connection(user, host, dport=22) :
     known_hosts = os.path.expanduser(os.path.join('~', '.ssh', 'known_hosts'))
     if os.path.exists(known_hosts) :
         ssh.load_host_keys(known_hosts)
+
     try :
         ssh.connect(host, port=dport, username=user)
     except paramiko.AuthenticationException :
-        passwd = getpass.getpass()
-        ssh.connect(host, port=dport, username=user, password=passwd)
+        if not _connect_with_password(ssh, user, host, dport) :
+            raise
+
     return ssh
