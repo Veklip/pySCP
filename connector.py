60ec0437fb04a8a586a6bb2fdedabdeb9d9162ac
Plamen Kolev <multyrealm@gmail.com>
2012-02-05 13:20:06 +0200

    add support for identity files to connector module

--- connector.py
+++ connector.py
@@ -6,12 +6,40 @@ import getpass
 import paramiko
 import policies
 
+def _load_pkey(key_file) :
+    fo = open(key_file, 'r')
+    line = fo.readline()
+    fo.close()
+
+    try :
+        pkey = None
+        if "RSA" in line :
+            try :
+                pkey = paramiko.RSAKey.from_private_key_file(key_file)
+            except paramiko.PasswordRequiredException :
+                sys.stderr.write("RSA key file '%s' requires a passphrase.\n" % key_file)
+                passwd = getpass.getpass()
+                pkey = paramiko.RSAKey.from_private_key_file(key_file, passwd)
+        elif "DSA" in line :
+            try :
+                pkey = paramiko.DSSKey.from_private_key_file(key_file)
+            except paramiko.PasswordRequiredException :
+                sys.stderr.write("DSA key file '%s' requires a passphrase.\n" % key_file)
+                passwd = getpass.getpass()
+                pkey = paramiko.DSSKey.from_private_key_file(key_file, passwd)
+        return pkey
+    except paramiko.SSHException as sshex :
+        sys.stderr.write("Key file '%s' is invalid: %s\n" \
+                         % (key_file, ' '.join(sshex.args)))
+        return None
+
 def _connect_with_password(ssh, user, host, dport=22) :
     triesLeft = 3
     while triesLeft :
         try :
             passwd = getpass.getpass()
             ssh.connect(host, port=dport, username=user, password=passwd)
+            return True
         except paramiko.BadAuthenticationType as ex :
             if 'password' in ex.allowed_types or \
                'keyboard-interactive' in ex.allowed_types :
@@ -21,21 +49,27 @@ def _connect_with_password(ssh, user, host, dport=22) :
             sys.stderr.write("Bad Authentication: allowed types: %s\n" \
                              % (','.join(ex.allowed_types)))
             return False
-        return True
     else :
         return False
 
-def get_connection(user, host, dport=22) :
+def get_connection(user, host, dport=22, pkeys=None) :
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(policies.SSHWarningPolicy())
     known_hosts = os.path.expanduser(os.path.join('~', '.ssh', 'known_hosts'))
     if os.path.exists(known_hosts) :
         ssh.load_host_keys(known_hosts)
 
-    try :
-        ssh.connect(host, port=dport, username=user)
-    except paramiko.AuthenticationException :
+    _pkeys = pkeys if pkeys is not None else ()
+
+    for key in _pkeys :
+        try :
+            lkey = _load_pkey(key)
+            ssh.connect(host, port=dport, username=user, pkey=lkey)
+            break
+        except paramiko.AuthenticationException :
+            pass
+    else :
         if not _connect_with_password(ssh, user, host, dport) :
-            raise
+            raise Exception("Cannot connect")
 
     return ssh
