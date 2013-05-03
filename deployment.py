f649c23c96ececc151a5b6cbca9450eea697262f
Plamen Kolev <multyrealm@gmail.com>
2013-05-03 22:00:48 +0300

    add deployment module
    The deployment module packs the pyscp source in a zip file and sends it
    using scp to the remote /tmp directory.

---
+++ deployment.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+import tempfile
+import zipfile
+import error
+import transfer as tfr
+
+def deploy(ssh) :
+    sys.stdout.write("Deploying pyscp on remote\n")
+
+    stdin, stdout, stderr = ssh.exec_command("uname")
+    while (not stdout.channel.exit_status_ready()) :
+        time.sleep(5./60.) # 5s
+    if (stdout.channel.recv_exit_status() != 0) :
+        sys.stderr.write(stderr.read(-1))
+        return False
+    # TODO: add MacOS and other Unix systems
+    if (not "Linux" in stdout.read(-1)) :
+        sys.stderr.write("Deployment is supported only on Linux\n")
+        return False
+
+    tf = tempfile.NamedTemporaryFile(mode="wb", suffix=".zip", dir="/tmp", delete=True);
+    pzf = zipfile.PyZipFile(tf, mode="w")
+    pzf.writepy(os.path.dirname(sys.argv[0]))
+    pzf.close()
+
+    stdin, stdout, stderr = ssh.exec_command("scp -t /tmp/pyscp.zip")
+
+    ret = tfr.send(stdin, stdout, sys.stderr, sys.stdout, [tf.name], False, False)
+    if ret == error.E_OK or ret == error.E_END :
+        return True
+    else :
+        sys.stderr.write(stderr.readline())
+        return False
