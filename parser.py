d64ff905ae6103863471d504d990aae86fae1542
Plamen Kolev <multyrealm@gmail.com>
2014-12-26 19:02:42 +0200

    Rework sources/sink handling
    Paths are now contained in SCPPath objects. At first one object per
    path. Later paths from the same user/host pair are put in the same
    object.

--- parser.py
+++ parser.py
@@ -1,50 +1,68 @@
 import os
 import os.path
 import sys
+import socket
 import getpass
+import argparse
 
-def parse_paths(paths) :
-    parsed_paths = []
-    for path in paths :
-        host_path = path.split(':', 1)
-        if len(host_path) == 1 :
-            # local path
-            parsed_paths.append({'user':'current', 'host':'local', 'path':os.path.abspath(host_path[0])})
-        else :
-            # remote path
-            usr_host = host_path[0].split('@', 1)
-            if len(usr_host) == 1 :
-                parsed_paths.append({'user':'current', 'host':usr_host[0], 'path':host_path[1]})
-            else :
-                parsed_paths.append({'user':usr_host[0], 'host':usr_host[1], 'path':host_path[1]})
-    return parsed_paths
 
-def analyse_paths(paths) :
-    user = paths[0]['user']
-    host = paths[0]['host']
-    stripped = [paths[0]['path']]
+class SCPPath(object):
+    def __init__(self, user, host, path):
+        self.user = user
+        self.host = host
+        self.path = path
 
-    for i in range(1, len(paths) - 1) :
-        if user != paths[i]['user'] or host != paths[i]['host'] :
-            sys.stderr.write("Error: all source paths need to be from the same user and host\n")
-            return False, "", "", []
-        stripped.append(paths[i]['path'])
+    def __eq__(self, obj):
+        return self.user == obj.user and self.host == obj.host
 
-    if host == paths[-1]['host'] :
-        sys.stderr.write("Error: source and sink files need to be with different hosts\n")
-        return False, "", "", []
-    stripped.append(paths[-1]['path'])
+    def __repr__(self):
+        return "{}({}, {}, {})".format(type(self).__name__,
+                                       self.user, self.host, self.path)
 
-    if host == "local" :
-        if paths[-1]['user'] != 'current' :
-            return True, paths[-1]['user'], paths[-1]['host'], stripped
-        else :
-            return True, getpass.getuser(), paths[-1]['host'], stripped
-    else :
-        if user != 'current' :
-            return False, user, host, stripped
-        else :
-            return False, getpass.getuser(), host, stripped
+
+def parse_scp_path(arg):
+    userhost_path = arg.split(':', 1)
+    user = getpass.getuser()
+    host = socket.getfqdn()
+    path = userhost_path[-1]
+    if (len(userhost_path) == 2):
+        user_host = userhost_path[0].split('@', 1)
+        host = socket.getfqdn(user_host[-1])
+        if (len(user_host) == 2):
+            user = user_host[0]
+    return SCPPath(user, host, path)
+
+
+def group_scp_paths(paths):
+    groups = []
+    for path in paths:
+        try:
+            idx = groups.index(path)
+            groups[idx].path.append(path.path)
+        except ValueError:
+            groups.append(SCPPath(path.user, path.host, [path.path]))
+    return groups
+
+
+def normalise_paths(paths):
+    npaths = []
+    for path in paths:
+        path = os.path.abspath(path)
+        if (not os.path.exists(path)):
+            raise argparse.ArgumentError(None, '"{}" does not exist.'.format(path))
+        npaths.append(path)
+    return npaths
+
+
+def unique_paths(paths):
+    unq_paths = []
+    for path in paths:
+        if (path not in unq_paths):
+            unq_paths.append(path)
+        else:
+            sys.stdout.write('pyscp: warning: skipping duplicate: "{}".\n'.format(path))
+    return unq_paths
+
 
 def check_pkeys(pkey_files) :
     if pkey_files is None :
--- pyscp.py
+++ pyscp.py
@@ -2,6 +2,7 @@ import sys
 import time
 import argparse
 import logging
+import socket
 import parser as psr
 import connector as con
 import error
@@ -82,7 +83,7 @@ def _remote_recv(dir_path, rec, preserve, check_hash) :
 
 def _build_arg_parser() :
     parser = argparse.ArgumentParser(description="Python secure copy over ssh", prog="pyscp")
-    parser.add_argument("paths", action="store", nargs="+", default=[], help="[[user@]host1:]file1 ... [[user@]host2:]file2")
+    parser.add_argument("paths", action="store", nargs="+", default=[], type=psr.parse_scp_path, help="[[user@]host1:]file1 ... [[user@]host2:]file2")
     parser.add_argument("-f", action="store_true", default=None, help="source", dest="from_v")
     parser.add_argument("-t", action="store_true", default=None, help="destination", dest="to_v")
     parser.add_argument("-r", action="store_true", default=None, help="recursively copy directories", dest="rec")
@@ -111,55 +112,69 @@ def main() :
     if (verbose_level == 3) :
         logging.basicConfig(level=logging.NOTSET)
 
-    # these should only be called by the remote
     ret = 1
-    if args.from_v :
-        ret = _remote_send(args.paths, args.rec, args.preserve,
-                           args.check_hash)
-    elif args.to_v :
-        ret = _remote_recv(args.paths[0], args.rec, args.preserve,
-                           args.check_hash)
-    else :
-        paths = psr.parse_paths(args.paths)
+    if (args.from_v or args.to_v):
+        # only executed by the remote
+        paths = [path.path for path in args.paths]
+        paths = psr.normalise_paths(paths)
+        paths = psr.unique_paths(paths)
+
+        if (args.from_v):
+            ret = _remote_send(paths, args.rec, args.preserve,
+                               args.check_hash)
+        elif (args.to_v):
+            ret = _remote_recv(paths[0], args.rec, args.preserve,
+                               args.check_hash)
+    else:
         # only executed by the local
-        if len(paths) < 2 :
-            parser.print_help()
-            return 1
-        send, user, host, paths = psr.analyse_paths(paths)
-        if len(host) == 0 :
+        localhost = socket.getfqdn()
+        paths = []
+        for path in psr.group_scp_paths(args.paths):
+            if (path.host == localhost):
+                path.path = psr.unique_paths(psr.normalise_paths(path.path))
+            paths.append(path)
+
+        sources = paths[:-1]
+        sink = paths[-1]
+
+        if (not sources):
+            parser.print_usage()
             return 1
 
-        if not psr.check_pkeys(args.pkeys) :
+        if (not psr.check_pkeys(args.pkeys)):
             return 1
 
-        try :
-            ssh = con.get_connection(user, host, args.port, args.pkeys)
-        except Exception as ex :
-            sys.stderr.write(str(ex) + '\n')
-            return 1
-
-        if args.quiet :
+        if (args.quiet):
             null = open(os.devnull, 'r+')
             local_out = sys.stdout
             local_err = sys.stderr
             sys.stdout = null
             sys.stderr = null
 
-        if send :
-            # from local to remote
-            ret = _local_send(ssh, paths[:-1], paths[-1],
-                              args.rec, args.preserve, args.check_hash)
-        else :
-            # from remove to local
-            ret = _local_recv(ssh, paths[:-1], paths[-1],
-                              args.rec, args.preserve, args.check_hash)
+        send = not sink.host == localhost
+        for src in sources:
+            if (send):
+                ssh = con.get_connection(sink.user, sink.host, args.port, args.pkeys)
+                # from local to remote
+                ret = _local_send(ssh, src.path, sink.path[0],
+                                  args.rec, args.preserve, args.check_hash)
+                ssh.close()
+            else:
+                # from remote to local
+                ssh = con.get_connection(src.user, src.host, args.port, args.pkeys)
+                ret = _local_recv(ssh, src.path, sink.path[0],
+                                  args.rec, args.preserve, args.check_hash)
+                ssh.close()
 
-        if args.quiet :
+        if (args.quiet):
             sys.stderr = local_err
             sys.stdout = local_out
             null.close()
-        ssh.close()
     return ret
 
-if __name__ == "__main__" :
-    exit(main())
+if (__name__ == "__main__"):
+    try:
+        exit(main())
+    except Exception as ex:
+        sys.stderr.write(str(ex) + '\n')
+        exit(1)
